34bc62ddba019308009633aaf0c8a278a093da44
Tanisha23 <77339985+Tanisha23@users.noreply.github.com>
2021-07-12 17:02:30 +0530

    Add files via upload

---
+++ c36/asynchronousBallMovement/sketch.js
@@ -0,0 +1,23 @@
+var canvas;
+var position,database;
+var gameState=0;
+var playerCount;
+var form,player,game;
+
+
+function setup(){
+    createCanvas(500,500);
+
+
+    database=firebase.database();
+
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    background("yellow")
+  
+  
+}
